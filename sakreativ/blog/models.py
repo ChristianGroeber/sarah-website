4cda4556e28c411d86d14ce5defbe4461efb934c
ChristianGroeber <christian.groeber@bluewin.ch>
2019-03-26 14:39:43 +0100

    modeld done

--- sakreativ/blog/models.py
+++ sakreativ/blog/models.py
@@ -8,3 +8,11 @@ class Post(models.Model):
     date_posted = models.DateTimeField()
     title = models.CharField(max_length=255)
     text = HTMLField('Content')
+    featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class PostList(models.Model):
+    posts = models.ManyToManyField(Post, limit_choices_to=20)
