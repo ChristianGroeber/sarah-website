23e7f202abdbb8ac554b7b33df51451e9ad0a9b4
ChristianGroeber <christian.groeber@bluewin.ch>
2019-03-27 10:32:09 +0100

    bug fixes

--- sakreativ/blog/views.py
+++ sakreativ/blog/views.py
@@ -5,7 +5,11 @@ from .models import Post
 
 def index(request):
     posts = Post.objects.all()
-    featured_post = Post.objects.get(featured=True)
+    featured_post = None
+    try:
+        featured_post = Post.objects.get(featured=True)
+    except Exception:
+        print('there\'s no featured post')
     return render(request, 'blog/index.html', {'posts': posts, 'featured_post': featured_post})
 
 
