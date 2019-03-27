82a5a293e63382cb8617396a48066a55b1fdfcf6
ChristianGroeber <christian.groeber@bluewin.ch>
2019-03-27 10:21:57 +0100

    added filebrowser

--- sakreativ/sakreativ/urls.py
+++ sakreativ/sakreativ/urls.py
@@ -14,11 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from filebrowser.sites import site
+
+from . import settings
 
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^admin/filebrowser/', site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
