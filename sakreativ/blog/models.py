ab15d320e98b84a2ab4686ea4cfdf534f6bcc439
ChristianGroeber <christian.groeber@bluewin.ch>
2019-03-28 16:43:48 +0100

    added context processors

--- sakreativ/blog/models.py
+++ sakreativ/blog/models.py
@@ -34,3 +34,6 @@ class Page(models.Model):
     title = models.CharField(max_length=50)
     link = models.CharField(max_length=200)
     show_on_page = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
